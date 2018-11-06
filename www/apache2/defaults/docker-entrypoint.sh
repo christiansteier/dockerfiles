7c39511b1f9100bee700659f433991196d9242b3
root <stuff@steier.xyz>
2018-11-06 10:16:38 +0000

    Added docker-entrypoint.sh

---
+++ www/apache2/defaults/docker-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+SERVERNAME=${SERVERNAME:-localhost}
+
+if [ ! -d /usr/local/apache2/sites ]; then
+  mkdir -p /usr/local/apache2/sites
+  chown www-data:root /usr/local/apache2/sites
+fi
+
+if ls /usr/local/apache2/sites/* > /dev/null 2>&1
+then
+  echo "[i] Site(s) for webserver found."
+else
+  echo "[i] Create example site in /usr/local/apache2/sites."
+  cp /usr/local/apache2/conf/extra/httpd-vhosts.conf /usr/local/apache2/sites/httpd-vhosts.conf 
+  chown www-data:www-data /usr/local/apache2/sites/httpd-vhosts.conf
+fi
+
+# Set server name.
+sed -i 's/%SERVERNAME%/'"${SERVERNAME}"'/' /usr/local/apache2/sites/httpd-vhosts.conf
+echo "Configured server name to ${SERVERNAME}."
+
+exec "$@"
