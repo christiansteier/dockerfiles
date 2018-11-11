c746531ed5e4046d57692a6aec767892493b9520
root <root@soz-kult.hs-duesseldorf.de>
2018-11-11 14:43:43 +0000

    Remove some modules

--- webapps/etherpad/v1.7/scripts/ep_modules.sh
+++ webapps/etherpad/v1.7/scripts/ep_modules.sh
@@ -6,8 +6,6 @@ echo -e "\n\n[i] Install awesome plugins \n"
 npm install \
 	ep_adminpads \
 	ep_font_color \
-	ep_font_size \
-	ep_fullscreen \
         ep_headings2 \
 	ep_historicalsearch \
 	ep_horizontal_line \
@@ -16,7 +14,6 @@ npm install \
 	ep_offline_edit \
 	ep_page_view \
 	ep_spellcheck \
-	ep_tables2 \
 	ep_timesliderdiff
 
 exit 0
