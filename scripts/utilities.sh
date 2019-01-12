3cf3b5e49513940c182046d4938f2204b56e4241
Christian Steier <christiansteier@users.noreply.github.com>
2019-01-12 08:24:45 +0100

    Delete utilities.sh

--- scripts/utilities.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-source /scripts/buildconfig
-
-echo -e "\n[i] Install often used tools\n"
-$minimal_apt_get_install less bzip2 vim-tiny psmisc curl git-core rsync unzip software-properties-common lsb-release
-ln -s /usr/bin/vim.tiny /usr/bin/vim
-
-exit 0
